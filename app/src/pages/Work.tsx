bf44cc8ec7059e0fb9c47d4bce3ecd272e580763
naixsu <plnacua02@gmail.com>
2025-04-29 23:51:51 +0800

    Add contextBridge

--- app/src/pages/Work.tsx
+++ app/src/pages/Work.tsx
@@ -2,5 +2,15 @@ import React from 'react';
 
 
 export const Work:React.FC = () => {
-    return <div>This is work page content</div>
-}
+    const electron = (window as any).electron;
+
+    return (
+        <div>
+            This is work page content. <br />
+            The home directory is @ {electron.homeDir()}. <br />
+            The OS arch is {electron.arch()}. <br />
+            The version is {electron.osVersion()}. <br />
+            The name of the user is {electron.userInfo()}. <br />
+        </div>
+    );
+};
