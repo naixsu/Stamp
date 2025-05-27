919d39ab7bec34a252b55fab62e9ecb0619786be
Paul Lexus Nacua <plnacua02@gmail.com>
2025-05-27 12:03:05 +0800

    Integrate to backend

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -3,10 +3,20 @@
     <body>
         {{ testMessage }}
     </body>
+    <p>{{ message }}</p>
 </template>
 
 <script setup>
+    import { ref, onMounted } from 'vue'
+
     const props = defineProps(['testMessage'])
+    const message = ref('Loading...')
+
+    onMounted(async () => {
+        const res = await fetch('http://127.0.0.1:8000/api/hello/')
+        const data = await res.json()
+        message.value = data.message
+    })
 </script>
 
 <style scoped>
